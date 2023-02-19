164f6d70bcd50554c57eb0fa3558319770db5fa9
kessmith <skeshawn86@gmail.com>
2023-02-19 11:50:00 -0500

    Committing logic associated with testing custom Yolov8 objecet detector

---
+++ object_detector.py
@@ -0,0 +1,6 @@
+from ultralytics import YOLO
+from ultralytics.yolo.v8.detect.predict import DetectionPredictor
+import cv2
+
+model = YOLO('weights/best.pt')
+model.predict(source='0', show=True, conf=0.5)
