3912a7d0be42c37a14704b8c05d672f6e90d5223
Keshawn <skeshawn86@gmail.com>
2023-03-17 18:17:34 -0400

    Resolve error with static_image_detector.py and successfully ran test to validate

--- test/static_image_detector.py
+++ test/static_image_detector.py
@@ -2,17 +2,17 @@ import torch
 
 # Import the model
 ## Loading in Local Model
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='/Users/kessmith/Documents/Projects/Thesis/NN_RealTime_Detector/weights/best.pt', force_reload=True)
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='weights\weight_v2.pt', force_reload=True)
 
 ## Loading in Local yolov5 repo
-model = torch.hub.load('/Users/kessmith/Documents/Projects/Thesis/yolov5', 'custom', path='/Users/kessmith/Documents/Projects/Thesis/NN_RealTime_Detector/weights/best.pt', source='local')
+model = torch.hub.load('yolov5', 'custom', path="weights\weight_v2.pt", source='local')
 
 # Static Image validation
-img = ['resources/sample_image.jpg']
-
+img = ['resources/test_image_01.jpg', 'resources/test_image_02.jpg','resources/test_image_03.jpg','resources/test_image_04.jpg','resources/test_image_05.jpg']
+# img = 'resources/test_image_01.jpg'
 # Inference
 results = model(img)
 
 # Results
 results.print()
-results.show()
+print(results.pandas().xyxy[0])
