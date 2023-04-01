d87344ae1c2d82eb1cf850f7c65d7159e9291b46
Keshawn <skeshawn86@gmail.com>
2023-03-31 21:47:31 -0400

    Update dynamic_image_detector.py with new logic

--- Test/dynamic_image_detector.py
+++ Test/dynamic_image_detector.py
@@ -1,36 +1,26 @@
-# Importing Libraries
+# Import Libraries
 import cv2
-import os
 import torch
-from PIL import Image
-import io
-import numpy as np
 
 cap = cv2.VideoCapture(0)
 
+# model = torch.hub.load('ultralytics/yolov5', 'yolov5s', force_reload=True)
+# Insert Yolov5 model
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='weights\weight_v2.pt')
+
+model = torch.hub.load('yolov5', 'custom', path="weights\weight_v2.pt", source='local')
+
 while 1:
-    # Reading in frames from camera
-    success, img = cap.read()
-    # Converting each frame to gray scale
-    # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
-    # Import in the Yolo model
-    model = torch.hub.load('ultralytics/yolov5', 'custom', path='weights\weight_v2.pt', force_reload=True) # Load local model weights
-    model = torch.hub.load('yolov5', 'custom', path="weights\weight_v2.pt", source='local', force_reload=True) # Load Local yolov repo
-    
-    # This is where the custom code begins.
-    if success == True:
-        ret,buff = cv2.imencode('.jpg', img)
-        img = buff.tobytes()
+    ret,img = cap.read()
         
-        frame = Image.open(io.BytesIO(img))
-        results = model(frame)
-        results.print()
-        
-        frame = np.squeeze(results.render())
-        img_BGR = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-    else:
+    cv2.imshow('img', img)
+    results = model(img)
+    print('Here are your results loop ', results)
+    results.print()
+
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
         break
-    
-    # cv2.imshow('img', img)
-    frame = cv2.imencode('.jpg', img_BGR)[1].tobytes()
+
+cap.release()
+cv2.destroyAllWindows()
