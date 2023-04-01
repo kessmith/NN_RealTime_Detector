1fcaf74fbd81bd9e660274ea08b7d988967e2928
Keshawn <skeshawn86@gmail.com>
2023-04-01 17:55:59 -0400

    Updated logic to render bounding boxes and labels when Output "image" is displayed

--- Test/dynamic_image_detector.py
+++ Test/dynamic_image_detector.py
@@ -2,20 +2,26 @@
 import cv2
 import torch
 
+# Assigns local webcam to cap variable
 cap = cv2.VideoCapture(0)
 
 # model = torch.hub.load('ultralytics/yolov5', 'yolov5s', force_reload=True)
 # Insert Yolov5 model
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='weights\weight_v2.pt')
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='weights\weight_v2.pt', force_reload=True)
 
 model = torch.hub.load('yolov5', 'custom', path="weights\weight_v2.pt", source='local')
 
 while 1:
     ret,img = cap.read()
-        
-    cv2.imshow('img', img)
+
+    # Running inference on detected images with current weights
     results = model(img)
-    print('Here are your results loop ', results)
+    # Takes original img and updates it with bounding boxes and labels
+    results.render()
+
+    # Creates display screen showing livestreaming of images detected
+    cv2.imshow('Output', img)
+    # Prints the results of inference (detected items) to the terminal
     results.print()
 
     k = cv2.waitKey(30) & 0xff
