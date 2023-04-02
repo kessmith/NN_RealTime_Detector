6d3ab9c2996240dbcdea7448f85e53a329a4abbb
Keshawn <skeshawn86@gmail.com>
2023-04-02 15:29:20 -0400

    Extracted coordinate details from results variable and printed to terminal

--- Test/dynamic_image_detector.py
+++ Test/dynamic_image_detector.py
@@ -22,8 +22,13 @@ while 1:
     # Creates display screen showing livestreaming of images detected
     cv2.imshow('Output', img)
     # Prints the results of inference (detected items) to the terminal
-    results.print()
+    # results.print()
+    x_min = results.pandas().xyxy[0]['xmin']
+    x_max = results.pandas().xyxy[0]['xmax']
+    y_min = results.pandas().xyxy[0]['ymin']
+    y_max = results.pandas().xyxy[0]['ymax']
 
+    print(x_min[0], x_max[0], y_min[0], y_max[0])
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
