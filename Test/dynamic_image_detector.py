6c86c17f31183225257bd45f27a3f6bcda5e6dc2
Keshawn <skeshawn86@gmail.com>
2023-04-07 08:00:44 -0400

    Setup intrinsic_extrsinic.py class which will handlle alll parametric conversion
    Logic fully setup and ready to begin full testing.

--- Test/dynamic_image_detector.py
+++ Test/dynamic_image_detector.py
@@ -1,6 +1,12 @@
 # Import Libraries
+import os
 import cv2
 import torch
+import pandas as pd
+
+from intrinsic_extrinsic import slamLogicHandler
+current_directory = os.path.dirname(os.path.abspath(__file__))
+coordinateConverter = slamLogicHandler()
 
 # Assigns local webcam to cap variable
 cap = cv2.VideoCapture(0)
@@ -24,11 +30,33 @@ while 1:
     # Prints the results of inference (detected items) to the terminal
     # results.print()
     x_min = results.pandas().xyxy[0]['xmin']
-    x_max = results.pandas().xyxy[0]['xmax']
     y_min = results.pandas().xyxy[0]['ymin']
+    x_max = results.pandas().xyxy[0]['xmax']
     y_max = results.pandas().xyxy[0]['ymax']
 
-    print(x_min[0], x_max[0], y_min[0], y_max[0])
+    if x_min.empty is False:
+        # Normallizing the above values -> Giving them as a porition fo the whole image
+        x = x_min[0]/x_max[0]
+        y = y_min[0]/y_max[0]
+        # width = int((x_max[0] - x_min[0]) / x_max[0])
+        # height = int((y_max[0] - y_min[0]) / y_max[0])
+
+        # Depth will be an entire equation: Based on position of camera's starting point.
+        # We will approximate the depth by gathering the values of x and y at the corner point of the testing area and use that as a sampling point
+        intrinsic_depth = 100 # This is for now
+
+        params = {
+            'intr_x': x,
+            'intr_y': y,
+            'depth': intrinsic_depth
+        }
+        # print(params)
+        ext_param_x, ext_params_y, ext_param_z = coordinateConverter.intrinsicParamConvert(params)
+        # print('x= ' + str(ext_param_x) + ' y= ' + str(ext_params_y) + ' z= ' + str(ext_param_z))
+        print('Converted coordinates to their world coordinates')
+
+        # Create some form of functionality to save my extrinsic and intrinsic parameters to an excel sheet
+
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
