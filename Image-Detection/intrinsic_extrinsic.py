e8a584dd4b2df54ce5456a1407a8d5d7d89d91b5
Keshawn <skeshawn86@gmail.com>
2023-06-23 07:53:11 -0400

    Algo update to include measured depth
    This PR contains the following updates:
    - New logic to incorporate the measured depth at each corner as well as the middle point in the frame
    - Logic to approximate depth for the "in-between" pixels
    - Logic clean --> Removing any unused code that was taking up space

--- Image-Detection/intrinsic_extrinsic.py
+++ Image-Detection/intrinsic_extrinsic.py
@@ -7,22 +7,19 @@ class slamLogicHandler:
     def __init__(self):
         pass
     
-    def intrinsicParamConvert(self, params, excelSave):
+    def intrinsicParamConvert(self, params):
         # Params De-construction
         pixel_coord = np.array([params['intr_x'], params['intr_y'], params['depth']])
         # focal_length = params['foc_length']
         print('2D Position: ', pixel_coord)
         camera_matrix = np.array([[427.340680, 0, 319.454590], [0, 426.1759614, 228.986480], [0, 0, 1]])
+
+        # Computes the invese of a camera matrix --> Resuls in the identity matrix of the original (camera_matrix) matrix
         inv_cam_matrix = np.linalg.inv(camera_matrix)
 
+        # Calculats the dot product using the inverse camera matrix and the pixel coordinates provided --> Produces the world (3D) coordinates of the originally detected image/frame
         world_coord = pixel_coord.dot(inv_cam_matrix)
         print('3D Coordinates: ', world_coord)
 
-        if excelSave:
-            self.writeToExcel(pixel_coord, world_coord)
+        # Returning the world coordinats back for the user to view
         return world_coord[0], world_coord[1], world_coord[2]
-
-#   This function may not be necessary and I may just need to figure out a new way to implement this idea
-    def writeToExcel(self, intrinsic, extrinsic):
-        pd.DataFrame([intrinsic, extrinsic]) \
-            .to_excel('Experiment_Results.xlsx', sheet_name='Single Robot Results')
