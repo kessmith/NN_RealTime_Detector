200e3fe89c1f0bea1892f73b151e9663d50daa96
Keshawn Smith <skeshawn86@gmail.com>
2024-01-08 16:06:18 -0500

    Updated intrinsic camera to take in coordinates from all cameras

--- Image-Detection/intrinsic_extrinsic.py
+++ Image-Detection/intrinsic_extrinsic.py
@@ -3,23 +3,37 @@ import cv2
 import numpy as np
 import pandas as pd
 
-class slamLogicHandler:
-    def __init__(self):
-        pass
-    
-    def intrinsicParamConvert(self, params):
-        # Params De-construction
-        pixel_coord = np.array([params['intr_x'], params['intr_y'], params['depth']])
-        # focal_length = params['foc_length']
-        print('2D Position: ', pixel_coord)
-        camera_matrix = np.array([[427.340680, 0, 319.454590], [0, 426.1759614, 228.986480], [0, 0, 1]])
+class SlamLogicHandler:
+    def __init__(self, num_cameras):
+        self.num_cameras = num_cameras
+        self.camera_matrices = []
 
-        # Computes the invese of a camera matrix --> Resuls in the identity matrix of the original (camera_matrix) matrix
-        inv_cam_matrix = np.linalg.inv(camera_matrix)
+    def addCameraMatrix(self, camera_matrix):
+        # Add camera matrix to the list
+        self.camera_matrices.append(camera_matrix)
 
-        # Calculats the dot product using the inverse camera matrix and the pixel coordinates provided --> Produces the world (3D) coordinates of the originally detected image/frame
-        world_coord = pixel_coord.dot(inv_cam_matrix)
-        print('3D Coordinates: ', world_coord)
+    def intrinsicParamConvert(self, pixel_coords_list):
+        if len(pixel_coords_list) != self.num_cameras:
+            raise ValueError("Number of pixel coordinate lists does not match the number of cameras.")
 
-        # Returning the world coordinats back for the user to view
-        return world_coord[0], world_coord[1], world_coord[2]
+        world_coords_list = []
+        
+        for i in range(self.num_cameras):
+            pixel_coord = np.array(pixel_coords_list[i])
+            
+            print(f'Camera {i + 1} - 2D Position: ', pixel_coord)
+
+            # Use the corresponding camera matrix for each camera
+            camera_matrix = np.array(self.camera_matrices[i])
+
+            # Computes the inverse of a camera matrix --> Results in the identity matrix of the original (camera_matrix) matrix
+            inv_cam_matrix = np.linalg.inv(camera_matrix)
+
+            # Calculates the dot product using the inverse camera matrix and the pixel coordinates provided --> Produces the world (3D) coordinates of the originally detected image/frame
+            world_coord = pixel_coord.dot(inv_cam_matrix)
+            print(f'Camera {i + 1} - 3D Coordinates: ', world_coord)
+
+            world_coords_list.append(world_coord)
+
+        # Returning the world coordinates back for the user to view
+        return world_coords_list
